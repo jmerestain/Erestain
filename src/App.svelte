8cb1e19ddac31685ee19f172e09e59279f1dbed9
jmerestain <59800799+jmerestain@users.noreply.github.com>
2020-10-14 04:35:13 +0800

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,99 @@
+<script>
+</script>
+
+<main class="h-full w-full min-w-screen min-h-screen px-4">
+	<div class="container mx-auto py-4 flex flex-col px-10">
+		<div class="flex flex-col gap-10">
+			<h1 class="text-primary text-center text-5xl select-none font-bantayog tracking-wider mt-5 font-bold">
+				ERESTAIN
+			</h1>
+			<div class="flex justify-center md:justify-between text-center text-xl gap-2
+			font-bold text-tertiary flex-wrap font-caption items-center select-none">
+				<div>
+					<p>Blockchain Engineering</p>
+				</div>
+				<div>
+					<p>Web Development</p>
+				</div>
+				<div>
+					<p>App Development</p>
+				</div>
+				<div>
+					<p>Technical Architecture</p>
+				</div>
+			</div>
+			<div class="flex justify-center flex-wrap gap-2">
+				<p class="text-primary text-3xl w-full mt-5 tracking-wide p-5 rounded-lg
+				leading-relaxed font-caption md:text-justify text-center mb-10 border-juris">
+					You can call me <span class="font-bold text-secondary">JM</span>.
+					I am a <span class="font-bold cunderline">full stack developer</span> from the Philippines;
+					currently a sophomore in the Ateneo De Manila University.
+				</p>
+			</div>
+		</div>
+		<div class="flex flex-wrap mt-10 justify-center lg:justify-between gap-4 items-center mb-10">
+			<div class="flex flex-col max-w-xl">
+				<img class="object-cover w-full h-64 object-top flex-shrink rounded-t-md grayscale"
+				src="./assets/img/punta_thumb.png" alt="Punta Landing Page" />
+				<p class="bg-secondary text-primary p-2 shadow-lg
+				text-center font-caption font-bold rounded-b-md text-lg">
+					Punta Shop
+				</p>
+			</div>
+			<div class="flex flex-col max-w-xl">
+				<img class="object-cover w-full h-64 object-center flex-shrink rounded-t-md grayscale"
+				src="./assets/img/memory-matrix_thumb.png" alt="Memory Matrix" />
+				<p class="bg-secondary text-primary p-2 shadow-lg
+				text-center font-caption font-bold rounded-b-md text-lg">
+					Memory Matrix
+				</p>
+			</div>
+		</div>
+	</div>
+</main>
+
+<style>
+.grayscale {
+	filter: grayscale(100%);
+}
+.font-caption {
+	font-family: 'Poppins', sans-serif;
+}
+@font-face {
+	font-family: Bantayog;
+	src: url('/assets/fonts/Bantayog-Regular.otf');
+}
+.cunderline {
+	border-bottom: 4px solid #087F8C;
+}
+.text-primary {
+	color: #f4f4f4;
+}
+.font-bantayog {
+	font-family: Bantayog;
+}
+.text-secondary {
+	color: #087F8C;
+}
+.bg-secondary {
+	background: #087F8C;
+}
+.text-tertiary {
+	color: #087F8C;
+}
+.text-quarnery {
+	color: #1a1c20;
+}
+.bg-card {
+	background: #f4f4f4;
+}
+.border-juris {
+	border: 2px solid #087F8C;
+}
+.linkies {
+	color: #1D3461;
+}
+main {
+	background: #0D090A;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+    defaultLineHeights: true,
+    standardFontWeights: true
+  },
+  purge: [
+    // "./src/**/*.svelte",
+  ],
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
